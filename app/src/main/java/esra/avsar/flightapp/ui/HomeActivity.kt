5fccdf1be0726405223277da2882a34c31d714f6
esra <ze.avsar@hotmail.com>
2021-06-11 13:19:26 +0300

    second commit

---
+++ app/src/main/java/esra/avsar/flightapp/ui/HomeActivity.kt
@@ -0,0 +1,12 @@
+package esra.avsar.flightapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import esra.avsar.flightapp.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
--- app/src/main/java/esra/avsar/flightapp/ui/LoginActivity.kt
+++ app/src/main/java/esra/avsar/flightapp/ui/LoginActivity.kt
@@ -1,13 +1,31 @@
 package esra.avsar.flightapp.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import esra.avsar.flightapp.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private val email = "appcent@appcent.mobi"
+    private val password = "123456"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btnLogin.setOnClickListener {
+            if (email == etEmail.text.toString() && password == etPassword.text.toString()) {
+                Toast.makeText(this, "Giriş Başarılı", Toast.LENGTH_SHORT).show()
+                val loginIntent = Intent(this, HomeActivity::class.java)
+                startActivity(loginIntent)
+                finish()
+            } else {
+                Toast.makeText(this,"Bilgileriniz hatalıdır. Lütfen bilgilerini kontrol ediniz.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
 
         //TODO: login tasarımı yapıldı, işlevi yazılacak.
     }
